a360ccc6b899548a43505aaf320b55e0a8e2192b
saptarshi-ghosh <santighosh321@gmail.com>
2024-07-31 01:13:58 +0530

    Updated

--- proj_basic/script.js
+++ proj_basic/script.js
@@ -1,3 +1,14 @@
+var crser = document.querySelector("#cursor")
+var crserb = document.querySelector("#cursor-back")
+
+
+document.addEventListener("mousemove",function(dets){
+    crser.style.left = dets.x+"px"
+    crser.style.top = dets.y+"px"
+    crserb.style.left = dets.x - 190+"px"
+    crserb.style.top = dets.y - 190+"px"
+})
+
 gsap.to("#nav",{
    backgroundColor :"#000",
    duration:0.5,
@@ -5,12 +16,24 @@ gsap.to("#nav",{
    scrollTrigger:{
         trigger:"#nav",
         scroller:"body",
-        markers: true,
+       
         start:"top -10%",
         end:"top -11%",
-        scrumb:2
+        scrub:2
     }
    
+})
+
+gsap.to("#main",{
+    backgroundColor: "#000",
+    scrollTrigger:{
+        trigger:"main",
+        scroller:"body",
+        markers: true,
+        start: "top -25%",
+        end: "top -70%",
+        scrub:2
+    }
 })
 
 
